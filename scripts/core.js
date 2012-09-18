b9ba5522b1888a544fd946d6a488afb65b9f3d16
alexkates <daniel.alexander.kates@gmail.com>
2012-09-18 08:05:24 -0400

    refactored. good spot to start scripting events

--- scripts/core.js
+++ scripts/core.js
@@ -1,4 +1,5 @@
 function start() {
+	getPageSize();
 	setInterval(update,1000/15);
 }
 
@@ -9,8 +10,71 @@ function update() {
 
 function clear() {
 	var canvas = document.getElementById("graphics");
-	canvas.height = canvas_height;
-	canvas.width = canvas_width;
+	canvas.height = canvasPoint.y;
+	canvas.width = canvasPoint.x;
 	var context = canvas.getContext("2d");
 	context.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function drawImage(imgSrc, imgX, imgY) {
+	var canvas = document.getElementById("graphics");
+	canvas.height = canvasPoint.y;
+	canvas.width = canvasPoint.x;
+	var context = canvas.getContext("2d");
+
+	var img = new Image();
+	img.onload = function () {
+		context.drawImage(img, imgX, imgY);
+	}
+	img.src = imgSrc;
+}
+
+function point(x, y) {
+	this.x = x;
+	this.y = y;
+	
+	this.toString = function() {
+		return "(" + this.x + ", " + this.y + ")";
+	}
+}
+
+function object(topLeft, img) {
+	this.topLeft = topLeft;
+	this.img = img;
+	
+	this.moveRight = function(step) {
+		this.topLeft.x += step;
+	}
+	
+	this.moveLeft = function(step) {
+		this.topLeft.x -= step;
+	}
+	
+	this.moveDown = function(step) {
+		this.topLeft.y += step;
+	}
+	
+	this.moveUp = function(step) {
+		this.topLeft.y -= step;
+	}
+	
+	this.willLeaveUp = function(step) {
+		return this.topLeft.y - step <= 10;		
+	}
+	
+	this.willLeaveLeft = function(step) {
+		return this.topLeft.x - step < 10;
+	}
+	
+	this.willLeaveRight = function(step) {
+		return this.topLeft.x + step >= canvasPoint.x;
+	}
+	
+	this.willLeaveDown = function(step) {
+		return this.topLeft.y + step >= canvasPoint.y;
+	}
+}
+
+function getPageSize() {
+  return new point(window.innerWidth, window.innerHeight);
 }
